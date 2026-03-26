7c39aa02f72ea3c41e0253ffedaedd5559924530
Ayşe Ayata <237328470+Ayse589@users.noreply.github.com>
2026-03-26 14:59:42 +0300

    Add files via upload

--- o4.java
+++ o4.java
@@ -1,35 +1,38 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class o4 {
     public static void main(String[] args) {
-        Scanner giris =new Scanner(System.in);
-        int sayi1,sayi2;
-        char islem;
-        double sonuc;
-        System.out.println("birinci sayiyi gir");
-        sayi1= giris.nextInt();
-        System.out.println("ikinci sayiyi gir");
-        sayi2= giris.nextInt();
-        System.out.println("islemi gir (+,-,*,/)");
-        islem= giris.next().charAt(0);
-        switch(islem){
-            case '+':
-                sonuc=(double)(sayi1+sayi2);
-                break;
-            case '-':
-                sonuc=(double)(sayi1-sayi2);
-                break;
-            case '*':
-                sonuc=(double)(sayi1*sayi2);
-                break;
-            case '/':
-                sonuc=(double)(sayi1/sayi2);
-                break;
-            default:
-                System.out.println("gecersiz islem");
-                return;
-        }
-        System.out.println("sonuc: "+sonuc);
+        int hak1;
+        int kasa=0;
+       
+        Scanner giris=new Scanner(System.in);
+        Random rnd=new Random(); 
+         int sayi1 = 1+ rnd.nextInt(9);
+         System.out.println(sayi1);
+         kasa+=sayi1;
+       
+         System.out.println("kac sefer daha tutayım");
+          hak1 =giris.nextInt();
+         for(int i=0;i<hak1;i++){
+             int sayi =  rnd.nextInt(9);
+             System.out.println(sayi);
+             kasa+=sayi;
+
+         }
+         if(kasa>35){
+            System.out.println("TEBRIKLER KAZANDINIZ");
+
+         }
+         else{
+            System.out.println("uzgunum kaybettiniz");
+         }
+
+
+
+
+
+
 
 
     }
